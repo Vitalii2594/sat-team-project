12f82639967f1c9f63db43e2332a798fe354f52d
yaroslawv-nexus <yaroslawv@gmail.com>
2023-04-26 18:55:12 +0300

    close-menu-anchor-nexus

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,7 +1,10 @@
 (() => {
     const mobileMenu = document.querySelector(".js-menu-container");
     const openMenuBtn = document.querySelector(".js-open-menu");
-    const closeMenuBtn = document.querySelector(".js-close-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const closeMenuLink1 = document.querySelector(".js-close-menu-link1");
+  const closeMenuLink2 = document.querySelector(".js-close-menu-link2");
+  const closeMenuLink3 = document.querySelector(".js-close-menu-link3");
 
     const toggleMenu = () => {
       const isMenuOpen =
@@ -16,7 +19,11 @@
     };
 
     openMenuBtn.addEventListener("click", toggleMenu);
-    closeMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuLink1.addEventListener("click", toggleMenu);
+  closeMenuLink2.addEventListener("click", toggleMenu);
+  closeMenuLink3.addEventListener("click", toggleMenu);
+  
 
     // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
